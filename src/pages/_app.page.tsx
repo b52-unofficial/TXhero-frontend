828a000521e8a4f2ed7020ea609e5ab8cf8ae215
Darron Park <darron1217@gmail.com>
2023-09-02 22:47:17 +0900

    chore: add page title

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -1,12 +1,12 @@
 import dayjs from 'dayjs';
 import duration from 'dayjs/plugin/duration';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 import { Provider } from 'react-redux';
 import { ToastContainer } from 'react-toastify';
 
 import { store } from '@/app/store';
 import WalletProvider from '@/components/WalletProvider';
-
 import '@/styles/globals.scss';
 import 'react-toastify/dist/ReactToastify.css';
 
@@ -16,6 +16,9 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
       <WalletProvider>
+        <Head>
+          <title>Transaction Hero</title>
+        </Head>
         <Component {...pageProps} />
         <ToastContainer />
       </WalletProvider>
